d20704884fa3bffa4946d3623f0f8cfce796b1e1
amanusk <amanusk@protonmail.com>
2018-12-22 21:01:27 +0200

    clean clean.sh

--- clean.sh
+++ clean.sh
@@ -5,8 +5,8 @@
 /bin/rm *.log
 
 /bin/rm -rf testdb/testdb*
-/bin/rm -rf testdb/blocks_ffldb/
-cp -r ~/.btcd/data/testnet/blocks_ffldb/ testdb/
+# /bin/rm -rf testdb/blocks_ffldb/
+# cp -r ~/.btcd/data/testnet/blocks_ffldb/ testdb/
 
 ## Delete all tables from databse
 #cockroach sql --format=csv -e 'SHOW TABLES FROM blockchain'  --insecure | tail -n +2   | xargs -n1 printf 'DROP TABLE blockchain."%s";\n'   | cockroach sql --insecure
